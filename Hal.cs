fa2662cec3ae178f8e5b0d5ab87d0343849c7a09
Jeweltek <bashaolu@gmail.com>
2019-02-15 13:02:52 -0500

    Finished Hal hover and object rotation

--- Hal.cs
+++ Hal.cs
@@ -1,3 +1,75 @@
-﻿sing System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
+public class Hal : MonoBehaviour
+{
+    public float hoverSpeed;
+    public float hoverHeight;
+    public Vector3 tempPosition;
+    GameObject target;
+
+
+    void Start()
+    {
+        tempPosition = transform.position;
+        target = GameObject.Find("Main Camera");
+        hoverSpeed = 0.7F;
+        hoverHeight = 0.07F;
+
+    }
+
+    void FixedUpdate()
+    {
+        VerticalHover();
+        TurnWithCamera();
+       
+    }
+
+    public void VerticalHover()
+    {
+        tempPosition.y = Mathf.Sin(Time.realtimeSinceStartup * hoverSpeed) * hoverHeight;
+        transform.position = tempPosition;
+    }
+
+    public void TurnWithCamera()
+    {
+        Vector3 targetPosition = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
+        transform.LookAt(targetPosition);
+    }
+
+  
+
+}
+//////// Hal Timer
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+    public float timer;
+    GameObject time;
+
+    void Start()
+    {
+
+     TimeTracker();
+    }
+
+    void FixedUpdate()
+    {
+        timer = Time.realtimeSinceStartup;
+    }
+
+    public void TimeTracker()
+    {
+        float minutes = Mathf.Floor(timer / 60);
+        float seconds = timer % 60;
+        time = GetComponent <Text Mesh> ()
+    }
+
+}
+
+
